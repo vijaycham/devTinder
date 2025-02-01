30742d5e9cbcb0df956b63c9abe35d83fc09c6cd
vijaycham <104141481+vijaycham@users.noreply.github.com>
2025-02-01 11:57:23 +0530

    Saved hardcoded user data to DB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "barath",
+    lastName: "M",
+    emailId: "barath@gmail.com",
+    password: "maha@123",
+  });
+
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (error) {
+    res.status(400).send("Error saving the user:" + error.message);
+  }
+});
 
 connectDB()
   .then(() => {
