8c95a5453b70bb75a771cb3f68bd85dabf9e8c34
vijaycham <104141481+vijaycham@users.noreply.github.com>
2025-02-02 09:17:13 +0530

    merge branch

--- src/app.js
+++ src/app.js
@@ -3,13 +3,12 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "barath",
-    lastName: "M",
-    emailId: "barath@gmail.com",
-    password: "maha@123",
-  });
+  console.log(req.body);
+  // creating a new instance of the user model
+  const user = new User(req.body);
 
   try {
     await user.save();
